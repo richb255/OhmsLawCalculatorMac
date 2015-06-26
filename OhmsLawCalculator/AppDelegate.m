82d70d729d1aeb4e8f017d72e674356a010ab32a
Richard Buttars <richardbuttars@Richards-Mac-mini.local>
2015-06-25 21:57:01 -0700

    Continued work on cleaning up and improving the code.

--- OhmsLawCalculator/AppDelegate.m
+++ OhmsLawCalculator/AppDelegate.m
@@ -10,7 +10,10 @@
 
 @interface AppDelegate ()
 
--(void)CalcCurrent;
+- (void) CalcCurrent;
+- (void) CalcVoltage;
+- (void) ClrEntries;
+- (void) CalcResistance;
 
 @property (weak) IBOutlet NSWindow *window;
 @end
@@ -27,12 +30,134 @@
 }
 
 - (IBAction)ClearEntries:(id)sender {
+//    _voltageBox.stringValue = @"";
+//    _currentBox.stringValue = @"";
+//    _resistanceBox.stringValue = @"";
+    [self ClrEntries];
+}
+
+- (IBAction)CalculateResistance:(id)sender {
+/*    double current = _currentBox.doubleValue;
+    double voltage = _voltageBox.doubleValue;
+    
+    
+    double resistance = voltage / current;
+    
+    if(1 == _ohmsRadio.integerValue){
+        
+    }else if(1 == _kiloOhmsRadio.integerValue){
+        if((resistance / 1000.0) > 1.0){
+            resistance /= 1000.0;
+        }
+        
+    }else if(1 == _megaOhmsRadio.integerValue){
+        if((resistance / 1000000.0) > 1.0){
+            resistance /= 1000000.0;
+        }
+    }
+    
+    _resistanceBox.doubleValue = resistance;
+*/
+    [self CalcResistance];
+}
+
+- (IBAction)CalculateCurrent:(id)sender {
+/*    double voltage = _voltageBox.doubleValue;
+    double resistance = _resistanceBox.doubleValue;
+    
+    if(1 == _ohmsRadio.integerValue){
+        
+    }else if(1 == _kiloOhmsRadio.integerValue){
+            resistance *= 1000.0;
+        
+    }else if(1 == _megaOhmsRadio.integerValue){
+            resistance *= 1000000.0;
+    }
+
+    double current = voltage / resistance;
+    
+    _currentBox.doubleValue = current;
+*/
+    [self CalcCurrent];
+}
+
+- (IBAction)CalculateVoltage:(id)sender {
+/*    double current = _currentBox.doubleValue;
+    double resistance = _resistanceBox.doubleValue;
+    
+    if(1 == _ohmsRadio.integerValue){
+        
+    }else if(1 == _kiloOhmsRadio.integerValue){
+            resistance *= 1000.0;
+    }else if(1 == _megaOhmsRadio.integerValue){
+            resistance *= 1000000.0;
+
+    }
+    
+    double voltage = current * resistance;
+    
+    _voltageBox.doubleValue = voltage;
+*/
+    [self CalcVoltage];
+}
+
+- (IBAction)mnuCalcVoltage:(id)sender {
+   [self CalcVoltage];
+}
+
+- (IBAction)mnuCalcCurrent:(id)sender {
+    [self CalcCurrent ];
+}
+
+/*
+  The methods below are the actual implementation of the calculations, the even handlers
+  just call these whenever actual work is to be done.
+*/
+
+- (void)CalcCurrent{
+    double voltage = _voltageBox.doubleValue;
+    double resistance = _resistanceBox.doubleValue;
+    
+    if(1 == _ohmsRadio.integerValue){
+        
+    }else if(1 == _kiloOhmsRadio.integerValue){
+        resistance *= 1000.0;
+        
+    }else if(1 == _megaOhmsRadio.integerValue){
+        resistance *= 1000000.0;
+    }
+    
+    double current = voltage / resistance;
+    
+    _currentBox.doubleValue = current;
+}
+
+- (void) CalcVoltage {
+    double current = _currentBox.doubleValue;
+    double resistance = _resistanceBox.doubleValue;
+    
+    if(1 == _ohmsRadio.integerValue){
+        
+    }else if(1 == _kiloOhmsRadio.integerValue){
+        resistance *= 1000.0;
+    }else if(1 == _megaOhmsRadio.integerValue){
+        resistance *= 1000000.0;
+        
+    }
+    
+    double voltage = current * resistance;
+    
+    _voltageBox.doubleValue = voltage;
+    
+}
+
+- (void) ClrEntries {
     _voltageBox.stringValue = @"";
     _currentBox.stringValue = @"";
     _resistanceBox.stringValue = @"";
 }
 
-- (IBAction)CalculateResistance:(id)sender {
+- (void) CalcResistance {
     double current = _currentBox.doubleValue;
     double voltage = _voltageBox.doubleValue;
     
@@ -53,81 +178,7 @@
     }
     
     _resistanceBox.doubleValue = resistance;
-}
-
-- (IBAction)CalculateCurrent:(id)sender {
-    double voltage = _voltageBox.doubleValue;
-    double resistance = _resistanceBox.doubleValue;
     
-    if(1 == _ohmsRadio.integerValue){
-        
-    }else if(1 == _kiloOhmsRadio.integerValue){
-            resistance *= 1000.0;
-        
-    }else if(1 == _megaOhmsRadio.integerValue){
-            resistance *= 1000000.0;
-    }
-
-    double current = voltage / resistance;
-    
-    _currentBox.doubleValue = current;
-}
-
-- (IBAction)CalculateVoltage:(id)sender {
-    double current = _currentBox.doubleValue;
-    double resistance = _resistanceBox.doubleValue;
-    
-    if(1 == _ohmsRadio.integerValue){
-        
-    }else if(1 == _kiloOhmsRadio.integerValue){
-            resistance *= 1000.0;
-    }else if(1 == _megaOhmsRadio.integerValue){
-            resistance *= 1000000.0;
-
-    }
-    
-    double voltage = current * resistance;
-    
-    _voltageBox.doubleValue = voltage;
-}
-- (IBAction)mnuCalcVoltage:(id)sender {
-    double current = _currentBox.doubleValue;
-    double resistance = _resistanceBox.doubleValue;
-    
-    if(1 == _ohmsRadio.integerValue){
-        
-    }else if(1 == _kiloOhmsRadio.integerValue){
-        resistance *= 1000.0;
-    }else if(1 == _megaOhmsRadio.integerValue){
-        resistance *= 1000000.0;
-        
-    }
-    
-    double voltage = current * resistance;
-    
-    _voltageBox.doubleValue = voltage;
-}
-
-- (IBAction)mnuCalcCurrent:(id)sender {
-    [self CalcCurrent ];
-}
-
-- (void)CalcCurrent{
-    double voltage = _voltageBox.doubleValue;
-    double resistance = _resistanceBox.doubleValue;
-    
-    if(1 == _ohmsRadio.integerValue){
-        
-    }else if(1 == _kiloOhmsRadio.integerValue){
-        resistance *= 1000.0;
-        
-    }else if(1 == _megaOhmsRadio.integerValue){
-        resistance *= 1000000.0;
-    }
-    
-    double current = voltage / resistance;
-    
-    _currentBox.doubleValue = current;
 }
 
 @end
